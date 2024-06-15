d7d4fb00671882613807b11a64b863fb77704b82
ditinagrawal <ditin.agrawal05@gmail.com>
2024-06-15 16:40:03 +0530

    Add Login

--- actions/register.ts
+++ actions/register.ts
@@ -1,7 +1,10 @@
 "use server";
 
 import { RegisterSchema } from "@/schemas";
-import * as z from "zod"
+import * as z from "zod";
+import bcrypt from "bcryptjs";
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
     const validatedFields = RegisterSchema.safeParse(values)
@@ -9,5 +12,21 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
         return { error: "Invalid fields" }
     }
 
+    const { name, email, password } = validatedFields.data
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    const existingUser = await getUserByEmail(email)
+
+    if (existingUser) return { error: "Email already in use!" };
+
+    await db.user.create({
+        data: {
+            name,
+            email,
+            password: hashedPassword
+        }
+    })
+
+    // TODO: Send email verification
     return { success: "Account created successfully!" }
 }
